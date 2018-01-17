0bfc10334412a46b2563aca9f2fd27b8f613b7b8
zcx <zcx@umich.edu>
2018-01-17 14:21:25 -0500

    add new option to use the number of non-redundant sequences to approximate neff

--- calNf.cpp
+++ calNf.cpp
@@ -14,6 +14,12 @@ const char* docstring=""
 "calNf seq.aln 0.8 0 128\n"
 "    The fourth optional argument is target Nf. Stop Nf calculation if\n"
 "    it is already greater than target Nf. default is 0 (no target Nf)\n"
+"\n"
+"calNf seq.aln 0.8 0 128 1\n"
+"    The fifth optional argument is whether to approximate Nf by number\n"
+"    of non-redundant sequences. This algorithm has time complexity O(N*C)\n"
+"    wheras the standard protocol has time complexity O(N*N), where N and\n"
+"    are the number of non-redundant sequences\n"
 ;
 
 #include <iostream>
@@ -28,17 +34,19 @@ using namespace std;
 string aa_list="ACDEFGHIKLMNPQRSTVWY";
 
 float calNf(const string infile="-", float id_cut=0.8, int norm=0,
-    float target_Nf=0)
+    float target_Nf=0, int filter_aln_only=0)
 {
     int L=0; // alignment length
     int i,n,m; // position, sequence index, sequence index
     vector <int> is_not_gap_vec; // if a positions is an amino acid 
     vector<vector<int> > is_not_gap_mat; // if a position is an amino acid
     vector<int> Lali_vec;  // length of aligned residues for each sequence
+    int Lali=0,Liden=0;
     string sequence;
     vector <string> aln;
     int norm_by_ali=(norm/10);
     norm %=10;
+    float seqID=0;
 
     /* read aln file */
     ifstream fp;
@@ -53,39 +61,63 @@ float calNf(const string infile="-", float id_cut=0.8, int norm=0,
         {
             L=sequence.length();
             is_not_gap_vec.assign(L,0);
+            /* normalize target Nf */
+            if (norm==0) target_Nf*=sqrt(L);
+            else if (norm==1) target_Nf*=L;
         }
         if (norm_by_ali==1)
         {
-            Lali_vec.push_back(0);
+            Lali=0;
             for (i=0;i<L;i++)
             {
                 if (aa_list.find(sequence[i])!=string::npos)
                 {
                     is_not_gap_vec[i]=1;
-                    Lali_vec[Lali_vec.size()-1]+=1;
+                    Lali+=1;
                 }
-                else
-                    is_not_gap_vec[i]=0;
+                else is_not_gap_vec[i]=0;
             }
-            is_not_gap_mat.push_back(is_not_gap_vec);
+            if (filter_aln_only==0)
+            {
+                is_not_gap_mat.push_back(is_not_gap_vec);
+                Lali_vec.push_back(Lali);
+            }
         }
-        aln.push_back(sequence);
+        if (filter_aln_only)
+        {
+            if (aln.size()>target_Nf) break;
+            seqID=0;
+            for (m=0;m<aln.size();m++)
+            {
+                Liden=0;
+                for (i=0;i<L;i++)
+                {
+                    Liden+=(aln[m][i]==sequence[i] && (norm_by_ali==0
+                        ||(is_not_gap_mat[m][i]*is_not_gap_vec[i])));
+                }
+                seqID=Liden/((norm_by_ali==0)?L:Lali);
+                if (seqID>id_cut) break;
+            }
+            if (seqID>id_cut) break; // find homolog, do not add to aln
+            if (norm_by_ali==1)
+            {
+                is_not_gap_mat.push_back(is_not_gap_vec);
+                Lali_vec.push_back(Lali);
+            }
+        }
         if (sequence.size()!=L)
         {
             cerr<<"ERROR! length not match for sequence "<<aln.size();
             exit(0);
         }
+        aln.push_back(sequence);
     }
     fp.close();
     is_not_gap_vec.clear();
 
-    /* normalize target Nf */
-    if (norm==0) target_Nf*=sqrt(L);
-    else if (norm==1) target_Nf*=L;
-
     /* for simple seq number counting */
     int seq_num=aln.size();
-    if (norm_by_ali==2)
+    if (norm_by_ali==2 || filter_aln_only)
     {
         if (norm==0) return 1.*seq_num/sqrt(L);
         else if (norm==1) return 1.*seq_num/L;
@@ -94,7 +126,6 @@ float calNf(const string infile="-", float id_cut=0.8, int norm=0,
 
     /* compute seqID */
     float Nf=0;
-    int Liden=0;
     vector<int> inv_seq_weight_vec(seq_num,1);
     for (m=0;m<seq_num;m++)
     {
@@ -136,6 +167,8 @@ int main(int argc, char **argv)
     float id_cut=0.8; // defined by gremlin
     int norm=0; // 0 - L^0.5, 1 - L, 2 - no normalize
     float target_Nf=0;
+    int filter_aln_only=0; // whether approximate Nf by the number of
+                           // non-redundant sequences
     if(argc<2)
     {
         cerr<<docstring;
@@ -145,6 +178,7 @@ int main(int argc, char **argv)
     if (argc>2) id_cut=atof(argv[2]);
     if (argc>3) norm=atoi(argv[3]);
     if (argc>4) target_Nf=atof(argv[4]);
-    cout<<calNf(infile,id_cut,norm,target_Nf)<<endl;
+    if (argc>5) filter_aln_only=atoi(argv[5]);
+    cout<<calNf(infile,id_cut,norm,target_Nf,filter_aln_only)<<endl;
     return 0;
 }
